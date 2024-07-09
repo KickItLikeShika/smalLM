dab0c4e6922588ee5be4507954a4b630b5324906
KickItLikeShika <ahmedkhaled19701@gmail.com>
2024-07-09 06:50:05 +0000

    updated optimizer params

--- train/main.py
+++ train/main.py
@@ -235,7 +235,7 @@ print(model)
 # and we are gettin 11.06 as an inital loss, which is still in the range!
 # print(loss)
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8)
 for i in range(50):
     t0 = time.time()
     x, y = train_loader.next_batch()
