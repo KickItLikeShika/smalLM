01b6583ba4d692e485aebc01a8519b9755c0556f
KickItLikeShika <ahmedkhaled19701@gmail.com>
2024-07-09 17:08:52 +0000

    implemented gradient accumulation

--- train/main.py
+++ train/main.py
@@ -241,6 +241,14 @@ torch.manual_seed(1337)
 if torch.cuda.is_available():
     torch.cuda.manual_seed(1337)
 
+total_batch_size = 524288  # 2**19, 0.5M, in number of tokens
+B = 8  # micro batch size
+T = 1024  # sequence length
+assert total_batch_size % (B * T) == 0, "make sure total_batch_size is divisible by B * T"
+grad_accum_steps = total_batch_size // (B * T)
+print(f"total desired batch size: {total_batch_size}")
+print(f"=> calculated gradient accumulation steps: {grad_accum_steps}")
+
 # init dataloader
 train_loader = DataLoaderLite(B=8, T=1024)
 
@@ -288,17 +296,24 @@ optimizer = model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, dev
 
 for step in range(max_steps):
     t0 = time.time()
-
-    x, y = train_loader.next_batch()
-    x, y = x.to(device), y.to(device)
-
     optimizer.zero_grad()
+    loss_accum = 0.0
+    for micro_step in range(grad_accum_steps):
+        x, y = train_loader.next_batch()
+        x, y = x.to(device), y.to(device)
 
-    # amp
-    with torch.autocast(device_type=device, dtype=torch.bfloat16):
-        logits, loss = model(x, y)
+        # amp
+        with torch.autocast(device_type=device, dtype=torch.bfloat16):
+            logits, loss = model(x, y)
+            
+        # we have to scalre the loss to account for gradient accumulation,
+        # because the gradients just add on each successive backward().
+        # addition of gradients corresponds to a SUM in the objective, but
+        # instead of a SUM we want MEAN. scale th loss here so it comes out right
+        loss = loss / grad_accum_steps
+        loss_accum += loss.detach()
+        loss.backward()
 
-    loss.backward()
     norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
 
     # get new lr
@@ -311,11 +326,12 @@ for step in range(max_steps):
     torch.cuda.synchronize()
 
     t1 = time.time()
-    dt = (t1 - t0) * 1000  # time in ms
+    dt = (t1 - t0) # time in s
 
-    tokens_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
+    tokens_processed = train_loader.B * train_loader.T * grad_accum_steps
+    tokens_per_sec = tokens_processed / dt
 
-    print(f"step: {step:4d} | loss: {loss.item()} | lr: {lr:.4e} | norm: {norm:.4f} | dt: {dt:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
+    print(f"step: {step:4d} | loss: {loss_accum.item():.6f} | lr: {lr:.4e} | norm: {norm:.4f} | dt: {dt*1000:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
 
 
 import sys; sys.exit(0)
